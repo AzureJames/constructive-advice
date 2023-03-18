a9fc582ebe25416ac1c881b001a06a6df3052418
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2023-03-18 12:57:34 -0400

    footr2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,10 +44,8 @@ const Footer = () => {
 					</div>
 					<div className='footer-two'>
 						<h3>Disclaimer</h3>
-						<p className='smltxt'>.</p>
-						Intuit, QuickBooks, and QuickBooks ProAdvisor are registered trademarks of Intuit Inc
-						<p className='smltxt'>.</p>
-						Used with permission under the QuickBooks ProAdvisor Agreement
+						<p className='smltxt'>Intuit, QuickBooks, and QuickBooks ProAdvisor are registered trademarks of Intuit Inc</p>
+						<p className='smltxt'>Used with permission under the QuickBooks ProAdvisor Agreement</p>						
 					</div>
 				</div>
 
