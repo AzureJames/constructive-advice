d5fead0cf24085c03f30338a25c5a6c40333bcbc
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2023-03-21 12:24:30 -0400

    txt4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -92,12 +92,13 @@ function Home() {
 				url='URL for the post'
 				type=''
 				title='Planting Trees'
-				summary='Trees are our valuable friends.
-				Planting trees in vacant areas has the potential for millions of dollars of benefits in air quality, energy, property value, and removing CO2. 
-				To help care for our planet, Constructive Advice will donate 25 dollars to Re-tree NY for every client 
+				summary='Trees are our valuable friends, and
+				planting trees in vacant areas vastly improves quality of life in Western New York. 
+				They clean the air that we breathe, shade our houses from the summer sun, and remove disabling carbon dioxide from the atmosphere. Planting trees has the potential for millions of dollars of benefits in air quality, energy, property value, and removing CO2. 
+				So to help care for our community and planet, Constructive Advice will donate 25 dollars to Re-tree NY for every client 
 				signed onto Quickbooks Online!'
 				src={Placeholder}
-				alt='Alt for the image'
+				alt='green forest'
 			/>
 			
 			<div className='flex1200'>
