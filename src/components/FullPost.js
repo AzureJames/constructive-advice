77b452849dd731654917c0cb2b0706c0ec103fcc
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2023-03-21 14:42:11 -0400

    full2

--- src/components/FullPost.js
+++ src/components/FullPost.js
@@ -9,7 +9,7 @@ const FullPost = (props) => {
 			<div className='content'>
 				<p className='post-type'>{props.type}</p>
 				<h1 className='post-title'>{props.title}</h1>
-				<p className='post-summary'>{props.summary}</p>
+				<p className='post-summary' style={{marginBottom: '2rem'}}>{props.summary}</p>
 				<p className='post-summary'>{props.summary2}</p>
 
 				{/* <a
