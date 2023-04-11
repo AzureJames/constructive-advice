b018109652ed4629157fae8b90e1929923088c53
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2023-04-11 13:45:30 -0400

    p

--- src/components/DevBanner.js
+++ src/components/DevBanner.js
@@ -2,13 +2,14 @@ import '../scss/components/_banner.scss';
 // import img from '../img/whiterose.jpg';
 import img from '../img/contact-bg-sm.jpg';
 
-export default function DevBanner({title, tagline, image, svgColor}) {
+export default function DevBanner({title, tagline, tagline2, image, svgColor}) {
   return (
     <>
         <div className='devbanner'>
             <h1 className='title'>{title}</h1>
               <img src={img} width={375} height={250} alt="mountain forest"/>
             <p className='description'>{tagline}</p>
+            <p className='description'>{tagline2}</p>
         </div>
     </>
   )
